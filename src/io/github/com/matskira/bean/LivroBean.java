7e97bf188da5ae21f70fe48ef6475da5aa7876ca
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-12 17:07:52 -0300

    Criado componente de combobox com lista de autores e adição de autores

--- src/io/github/com/matskira/bean/LivroBean.java
+++ src/io/github/com/matskira/bean/LivroBean.java
@@ -1,17 +1,50 @@
 package io.github.com.matskira.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import io.github.com.matskira.dao.DAO;
+import io.github.com.matskira.model.Autor;
 import io.github.com.matskira.model.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
 
+	private List<Autor> autores = new DAO<Autor>(Autor.class).listaTodos();
+	
+	private List<Autor> autoresDoLivro = new ArrayList<Autor>();
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.getLivro().getAutores();
+	}
+
+	private Integer autorId;
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
 	public Livro getLivro() {
 		return livro;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.getAutorId());
+		this.getLivro().adicionaAutor(autor);
 	}
 
 	public void gravar() {
