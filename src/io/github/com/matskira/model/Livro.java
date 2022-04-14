82f3103f3212613a29b4db493934942f431ab517
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-04-14 16:27:51 -0300

    Adicionado AJAX nas nossas telas

--- src/io/github/com/matskira/model/Livro.java
+++ src/io/github/com/matskira/model/Livro.java
@@ -21,7 +21,7 @@ public class Livro {
 	private String titulo;
 	private String isbn;
 	private double preco;
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
 	private Calendar dataLancamento = Calendar.getInstance();
 
 	@ManyToMany
